b7b00e81b5516ed43e11e1ba52b71b4bb7f9cefd
Shuflduf <shuflduf@shuflduf.xyz>
2025-04-04 11:02:31 -0600

    NOTION

--- src/lib/formatNotionContent.ts
+++ src/lib/formatNotionContent.ts
@@ -1,19 +1,7 @@
 import type { NotionPage } from "./getNotionContent";
 
 const formatNotionContent = (content: NotionPage): string => {
-  console.log("FORMATTING")
-  const { name, description, pageContent, lastModified, createdAt } = content;
-
-  const formattedContent = `---
-title: ${name}
-description: ${description}
-pubDate: ${createdAt}
-modDate: ${lastModified}
----
-${pageContent}
-`;
-
-  return formattedContent;
+  return content.pageContent;
 }
 
 export default formatNotionContent;
