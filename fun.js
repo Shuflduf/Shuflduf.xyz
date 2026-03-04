458de5c42a0c8d71733bebb99e16abbf8f2dca8d
Shuflduf <shuflduf@gmail.com>
2026-03-03 22:16:06 -0700

    make commits link to the actual page

--- fun.js
+++ fun.js
@@ -4,13 +4,17 @@ $(document).ready(function () {
     function (commits) {
       const $list = $(".updates ul").empty();
       commits.forEach(function (c) {
+        console.log(c);
         const date = new Date(c.commit.author.date);
         const d = String(date.getDate()).padStart(2, "0");
         const m = String(date.getMonth() + 1).padStart(2, "0");
         const y = String(date.getFullYear()).slice(-2);
+        const href = String(c.html_url);
         const label = `${d}.${m}.${y}`;
         const msg = c.commit.message.split("\n")[0];
-        $list.append(`<li>${msg}<sub>(${label})</sub></li>`);
+        $list.append(
+          `<li>${msg}<sub><a href="${href}"">(${label})</a></sub></li>`,
+        );
       });
     },
   );
