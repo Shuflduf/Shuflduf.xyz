525384abf285f14d8f9ffb95779f34ba007e3700
Shuflduf <shuflduf@gmail.com>
2026-03-03 22:12:14 -0700

    make updates box consistent

--- fun.js
+++ fun.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $.getJSON(
     "https://api.github.com/repos/Shuflduf/Shuflduf.xyz/commits?per_page=5",
     function (commits) {
-      const $list = $(".updates").empty();
+      const $list = $(".updates ul").empty();
       commits.forEach(function (c) {
         const date = new Date(c.commit.author.date);
         const d = String(date.getDate()).padStart(2, "0");
@@ -10,7 +10,7 @@ $(document).ready(function () {
         const y = String(date.getFullYear()).slice(-2);
         const label = `${d}.${m}.${y}`;
         const msg = c.commit.message.split("\n")[0];
-        $list.append(`<li><sub>${label}</sub>${msg}</li>`);
+        $list.append(`<li>${msg}<sub>(${label})</sub></li>`);
       });
     },
   );
