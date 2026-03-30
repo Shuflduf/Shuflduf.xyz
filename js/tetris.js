8b2c0604de9525c7785fc1b770952c25177f12be
Shuflduf <shuflduf@gmail.com>
2026-03-30 09:54:29 -0600

    basic gameplay and 7bag implemented

--- js/tetris.js
+++ js/tetris.js
@@ -9,17 +9,19 @@ const COLOURS = [
   "#4455e7",
   "#f64ed8",
 ];
+const NEXT_PIECES = 1;
 const GRAVITY_TIME = 300;
 
 let canvas = null;
 let ctx = null;
 let tileSize = 0;
 
-let activePiece = {
-  index: 0,
-  pos: [3, 0],
-  rot: 0,
-};
+let board = Array.from(Array(BOARD_SIZE[0]), () =>
+  new Array(BOARD_SIZE[1]).fill(null),
+);
+let bag = [];
+let next = [];
+let activePiece = null;
 let gravityTimer = 0;
 
 $(function () {
@@ -27,6 +29,7 @@ $(function () {
   ctx = canvas.getContext("2d");
 
   console.log(ctx);
+  resetGame();
   addEventListener("keydown", handleInputs);
   requestAnimationFrame(process);
 });
@@ -42,7 +45,9 @@ function process(currentFrame) {
 
   gravityTimer += delta;
   if (gravityTimer > GRAVITY_TIME) {
-    activePiece.pos[1] += 1;
+    if (!tryMove([0, 1])) {
+      placePiece();
+    }
     gravityTimer = 0;
   }
 
@@ -52,8 +57,8 @@ function process(currentFrame) {
   requestAnimationFrame(process);
 }
 
-function drawTile(pos) {
-  ctx.fillRect((pos[0] + 1) * tileSize, pos[1] * tileSize, tileSize, tileSize);
+function drawTile([x, y]) {
+  ctx.fillRect((x + 1) * tileSize, y * tileSize, tileSize, tileSize);
 }
 
 function drawBoard() {
@@ -64,6 +69,18 @@ function drawBoard() {
   }
   for (let x = 0; x < BOARD_SIZE[0]; x++) {
     drawTile([x, BOARD_SIZE[1]]);
+  }
+
+  for (let x = 0; x < BOARD_SIZE[0]; x++) {
+    for (let y = 0; y < BOARD_SIZE[1]; y++) {
+      const blockIndex = board[x][y];
+      if (blockIndex == null) continue;
+
+      ctx.fillStyle = COLOURS[blockIndex];
+      drawTile([x, y]);
+      // fill(color(COLOURS[blockIndex]));
+      // tileAt(x, y);
+    }
   }
 }
 
@@ -80,10 +97,100 @@ function handleInputs(event) {
   console.log(event.code);
   switch (event.code) {
     case "KeyA":
-      activePiece.pos[0] -= 1;
+      tryMove([-1, 0]);
       break;
     case "KeyD":
-      activePiece.pos[0] += 1;
+      tryMove([1, 0]);
+      break;
+    case "KeyW":
+      tryMove([0, 1]);
       break;
   }
+}
+
+function placePiece() {
+  for (const pos of SRS.pieces[activePiece.index][activePiece.rot]) {
+    const blockPos = [activePiece.pos[0] + pos[0], activePiece.pos[1] + pos[1]];
+    board[blockPos[0]][blockPos[1]] = activePiece.index;
+  }
+  activePiece = nextPiece();
+  // clearLines();
+  // for (const pos of SRS.pieces[currentPiece.index][currentPiece.rot]) {
+  //   const blockPos = [
+  //     currentPiece.pos[0] + pos[0],
+  //     currentPiece.pos[1] + pos[1],
+  //   ];
+  //   if (!safePlace(blockPos[0], blockPos[1])) resetGame();
+  // }
+  gravityTimer = 0;
+  // justHeld = false;
+}
+
+function tryMove([dx, dy]) {
+  for (const pos of SRS.pieces[activePiece.index][activePiece.rot]) {
+    const testPos = [
+      activePiece.pos[0] + pos[0] + dx,
+      activePiece.pos[1] + pos[1] + dy,
+    ];
+    if (!safePlace(testPos)) return false;
+  }
+  activePiece.pos[0] += dx;
+  activePiece.pos[1] += dy;
+  return true;
+}
+
+function resetGame() {
+  board = Array.from(Array(BOARD_SIZE[0]), () =>
+    new Array(BOARD_SIZE[1]).fill(null),
+  );
+  bag = [];
+  next = [];
+  // justHeld = false;
+  // held = null;
+  for (let i = 0; i < NEXT_PIECES; i++) {
+    next.push(nextBagIndex());
+  }
+  activePiece = nextPiece();
+}
+
+function nextPiece() {
+  next.push(nextBagIndex());
+  return {
+    index: next.shift(),
+    pos: [3, 0],
+    rot: 0,
+  };
+}
+
+function nextBagIndex() {
+  if (bag.length == 0) {
+    bag = Array.from({ length: 7 }, (_, i) => i);
+    shuffle(bag);
+  }
+  return bag.pop();
+}
+
+function safePlace([x, y]) {
+  if (x < 0 || x >= BOARD_SIZE[0]) return false;
+  if (y >= BOARD_SIZE[1]) return false;
+  if (board[x][y] != null) return false;
+
+  return true;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
